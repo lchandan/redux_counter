056409a6b0ce4f47b9f94d5c19bd756507c5853f
Chandan Lakkoju <lchandan@outlook.com>
2021-04-12 22:37:41 +0530

    adding and committing index file
    Signed-off-by: Chandan Lakkoju <lchandan@outlook.com>

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom';
 
 import {Provider} from 'react-redux'
 import store from './redux/store';
 
-
 import Counter from './components/Counter'
-//import incrementReducer from './redux/increment/increment.reducer'
 
 const Sample =()=>(
   <div><b>Chandan First Redux Component</b></div>
